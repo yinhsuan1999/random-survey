ddcace45748b18e325a434d854224420fefbfcbd
yinhsuan1999 <123303851+yinhsuan1999@users.noreply.github.com>
2023-01-22 20:57:10 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,34 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://forms.gle/UzY72fjuiikJzVNp9',
+	'https://forms.gle/VirT6gXDH2YXdFUa8',
+	'https://forms.gle/DiYd6b9SQ3WdsuHt8',
+	'https://forms.gle/ee9tr7v1cGcVaaq6A',
+	'https://forms.gle/USTRk6VsCP6dd2JQ7',
+	'https://forms.gle/bXktxtK6qa3GQhKx6',
+	'https://forms.gle/h4AjxJAoVnokNzPm7',
+	'https://forms.gle/ZEE7aoXtgm4nRGNr7',
+	'https://forms.gle/yQNhXBbndG8RdYkA9',
+	'https://forms.gle/8FTQRdoKj6PbDhEw6',
+	'https://forms.gle/GT1gBw1wsHiLh9uE9',
+	'https://forms.gle/JoAHCz2XqHLVkyV3A',
+	'https://forms.gle/XAhZnwJRJyYfe33J9',
+	'https://forms.gle/dUS4nBcqj7Dbgk1X6',
+	'https://forms.gle/ceS1V9NVvCS1EvKL6',
+	'https://forms.gle/QeM2EZcBFugr19n79',
+	'https://forms.gle/bGE7A6NuYLESTHXR6',
+	'https://forms.gle/x38ANXqLXvgS9Mm29',
+	'https://forms.gle/6gPMWzBMWbL9A4bZ8',
+	'https://forms.gle/d8UKYCNiHaTqahq67',
+	'https://forms.gle/5nVDBJ1etSAsYEAP8',
+	'https://forms.gle/hBRJ4jqRcjiFVfaVA',
+	'https://forms.gle/YZy2MbkFJHrmtdQA8',
+	'https://forms.gle/rv6PVmoQvp1KQGCAA',
+	'https://forms.gle/3K5g7oNDYnwskdny7',
+	'https://forms.gle/BQZQbhY1WAdJZKQQ9',
+	'https://forms.gle/13oQ3QjnZ6AkZjh18',
+	'https://forms.gle/pPAwmm9cxbThUyKp8'
+	
+	
 ];
